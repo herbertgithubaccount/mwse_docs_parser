edf8213d4b36451266ae0f08b48694aa655b2959
herbert100 <154224164+herbertgithubaccount@users.noreply.github.com>
2025-03-08 19:24:06 +0100

    going to test out making `process_file` not async

--- src/main.rs
+++ src/main.rs
@@ -80,7 +80,10 @@ impl DirPackages {
 	}
 }
 
-
+/// Recursively processes a file and builds a `EPkg` from it.
+/// If there is a directory with the same name as `path`, then that directory will be recursively processed
+/// and any packages found there will be added to the package returned by this function.
+/// The `Box::pin` stuff is necessary for recursive async functions.
 fn process_file(path: PathBuf) -> Pin<Box<dyn Send + Sync + Future<Output = Result<EPkg, Error>>>> {
 	Box::pin(async move {
 			
@@ -95,29 +98,92 @@ fn process_file(path: PathBuf) -> Pin<Box<dyn Send + Sync + Future<Output = Resu
 		};
 
 
+		// Directory is what you get after removing the `.lua` extension from the filepath.
+		let dir = path.with_extension("");
 
-		let matching_dir = path.with_extension("");
+		// Recursively process the child directory if necessary
+		if dir.exists() && dir.is_dir() {
 
-		if matching_dir.exists() && matching_dir.is_dir() {
-			// things to ignore when processing the directory
-			let example_paths = match epkg.core().examples.as_ref() {
-				Some(examples) => examples.iter().map(|e| e.path.clone()).collect(),
-				None => HashSet::new(),
+			// process_dir(dir, ignore).await?
+			trace!("processing {dir:?}");
+			// Iterator over all the files in the directory.
+			let mut dir_contents = tokio::fs::read_dir(dir).await?;
+
+			// This stores all of the recursive calls to `process_file`.
+			// These are started up in new threads and executed asynchronously,
+			// then joined together at the end.
+			// Doing this asynchronously results in a massive speedup.
+			let mut file_handles = Vec::new();
+
+			// Any descendent `EPkg`s that should be merged into this one.
+			let mut children: Vec<EPkg> = Vec::new();
+
+
+
+			// Both arms of this branch are responsible for iterating over the directory
+			// and filling up `children`.
+			// Things are split up in order to optimize the code, since one branch skips over
+			// files that match the examples found in `epkg.core()`, and most files don't have examples.
+			match epkg.core().examples.as_ref() {
+				None => {
+					while let Some(entry) = dir_contents.next_entry().await? {
+						// directories are handled in recursive calls
+						if !entry.file_type().await?.is_file() { continue; }
+						// recursively process any files in this directory
+						// if let Ok(epkg) = process_file(entry.path()).await {
+						// 	children.push(epkg);
+						// }
+						file_handles.push(tokio::spawn(process_file(entry.path())));
+					}
+				},
+				// `epkg` has examples. We shouldn't parse any files that are intended to be examples.
+				Some(examples) => {
+					let to_ignore: HashSet<Box<str>> = examples.iter()
+						.map(|e| e.path.clone())
+						.collect();
+					while let Some(entry) = dir_contents.next_entry().await? {
+				
+						// directories are handled in recursive calls
+						if !entry.file_type().await?.is_file() { continue; }
+		
+						// don't even try to parse the examples
+						let path: PathBuf = entry.path().with_extension("");
+						let filename = path.file_name().unwrap().to_str().unwrap();
+						if to_ignore.contains(filename) {
+							continue;
+						}
+						// recursively process any files in this directory
+						// if let Ok(epkg) = process_file(entry.path()).await {
+						// 	children.push(epkg);
+						// }
+						file_handles.push(tokio::spawn(process_file(entry.path())));
+					}
+				}
 			};
-			let children = process_dir(matching_dir, example_paths).await?;
 
+			for file_handle in file_handles {
+				if let Ok(epkg) = file_handle.await? {
+					children.push(epkg);
+				}
+			}
+
+			// Add the processed children to the parent file, but only if there are any children to add.
+			// We are doing this `len` check in order to avoid triggering the `error`s in the
+			// bottom branches of the `match &mut epkg` statement.
 			if children.len() > 0 {
 				match &mut epkg {
-					EPkg::Class(cls_pkg) => for child in children {
-						match child {
-							EPkg::Function(pkg) => cls_pkg.functions.push(pkg),
-							EPkg::Method(pkg) => cls_pkg.methods.push(pkg),
-							EPkg::Operator(pkg) => cls_pkg.ops.push(pkg),
-							EPkg::Value(pkg) => cls_pkg.values.push(pkg),
-							
-							p => todo!("is this even possible? child = {:?}", p.core().name),
+					EPkg::Class(cls_pkg) => {
+						for child in children {
+							match child {
+								EPkg::Function(pkg) => cls_pkg.functions.push(pkg),
+								EPkg::Method(pkg) => cls_pkg.methods.push(pkg),
+								EPkg::Operator(pkg) => cls_pkg.ops.push(pkg),
+								EPkg::Value(pkg) => cls_pkg.values.push(pkg),
+								
+								p => todo!("is this even possible? child = {:?}", p.core().name),
+							}
+						
 						}
-					
 					},
 					EPkg::Lib(lib_pkg) => for child in children {
 						match child {
@@ -130,14 +196,12 @@ fn process_file(path: PathBuf) -> Pin<Box<dyn Send + Sync + Future<Output = Resu
 						}
 					},
 	
-					// p => todo!("is this even possible? parent = {:?}", p.core().name),
 					EPkg::Function(par) => todo!("is this even possible? par = Function({:?}). children.len() = {}", par.core.name.as_ref(), children.len()),
 					EPkg::Method(par) => todo!("is this even possible? par = Method({:?}). children.len() = {}", par.core.name.as_ref(), children.len()),
 					EPkg::Value(par) => todo!("is this even possible? par = Value({:?}). children.len() = {}", par.core.name.as_ref(), children.len()),
 					EPkg::Event(par) => todo!("is this even possible? par = Event({:?}). children.len() = {}", par.core.name.as_ref(), children.len()),
 					EPkg::Operator(par) => todo!("is this even possible? par = Operator({:?}). children.len() = {}", par.core.name.as_ref(), children.len()),
 				}
-				// return Some((epkg, path))
 			}
 		}
 	
@@ -150,43 +214,11 @@ fn process_file(path: PathBuf) -> Pin<Box<dyn Send + Sync + Future<Output = Resu
 
 /// Scans through `dir` and parses all packages defined with it. The `ignore` parameter specifies which
 /// filepaths should be skipped when iterating. It is obtained from parsing the `examples` parameter of the parent component.
-fn process_dir(dir: PathBuf, ignore: HashSet<Box<str>>) -> Pin<Box<dyn Send + Sync + Future<Output = Result<Vec<EPkg>, Error>>>> {
-	Box::pin(async move {
-		trace!("processing {dir:?}");
-		let mut stream = tokio::fs::read_dir(dir).await?;
-		let mut pkgs = Vec::new();
-
-		let mut file_handles = Vec::new();
-		info!("filtering with hashet = {:?}", ignore);
-		while let Some(entry) = stream.next_entry().await? {
-			
-			// Don't have to wait very long for this.
-			// And waiting for it allows us to split the futures up based on return type.
-			// This means less branching later on.
-			let ft = entry.file_type().await?;
-
-			// directories are handled in recursive calls
-			if !ft.is_file() {
-				continue;
-			}
-			// don't even try to parse the examples
-			let path: PathBuf = entry.path().with_extension("");
-			let filename = path.file_name().unwrap().to_str().unwrap();
-			if ignore.contains(filename) {
-				continue;
-			}
-
-			file_handles.push(tokio::spawn(process_file(entry.path())));
-		}
+// fn process_dir(dir: PathBuf, ignore: HashSet<Box<str>>) -> Pin<Box<dyn Send + Sync + Future<Output = Result<Vec<EPkg>, Error>>>> {
+// 	Box::pin(async move {
 		
-		for file_handle in file_handles {
-			if let Ok(epkg) = file_handle.await? {
-				pkgs.push(epkg);
-			}
-		}
-		return Ok(pkgs);
-	})
-}
+// 	})
+// }
 pub struct DocPackages {
 	pub types: Vec<ClassPkg>,
 	pub globals: Vec<EPkg>,
